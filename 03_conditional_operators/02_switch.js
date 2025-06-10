78bcad70bea3d19c7436f66e7a21ee60aeead06d
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-06-10 19:13:09 +0530

    add loops

---
+++ 03_conditional_operators/02_switch.js
@@ -0,0 +1,13 @@
+let num = 3
+
+switch (num) {
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    case 4: console.log('D');
+    break;
+    default: console.log('F');
+}

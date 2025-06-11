24fe1eb29a3155a845f51bcbf5df97f8ace9238e
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-06-11 20:33:25 +0530

    add strings folder

---
+++ 05_strings/01_strings.js
@@ -0,0 +1,13 @@
+let firstname = "Abhishek";
+let lastname = "Sharma";
+
+// let name = "this is 
+// abhishek 
+// here" //-- this will pop an error 
+
+
+let name = `This is 
+abhishek 
+here`; // by the help of this tilted notation we can print string with new line 
+
+console.log(name);

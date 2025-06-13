8ce4d7a7ccd0a75f7e340250849f359d8de410ef
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-06-13 21:54:19 +0530

    add referene datatypes

---
+++ 07_Reference_datatypes/01_arrays.js
@@ -0,0 +1,38 @@
+// Reference type-(stored in heap memory)
+//array 
+//object
+//functions
+
+
+// let fruits = ["Apple", "Banana", "Orange"];
+
+// fruits.push("Kiwi");
+
+// fruits.unshift("Grapes");
+
+// console.log(fruits);
+
+// // Output : [ 'Grapes', 'Apple', 'Banana', 'Orange', 'Kiwi' ]
+
+let arr = [1,2,3,4,5];
+let brr = new Array("Abhishek",1,true);
+brr.push("sharma");
+console.log(brr);
+brr.pop();
+console.log(brr);
+
+// Array operations-
+/*
+1.push-insert at end 
+2.pop-remove last itme
+3.shift-remove first element
+4.unshift-add element to first index
+5.slice
+6.splice
+7.map
+8.filter
+9.reduce
+10.sort
+11.indexof
+12.find
+*/
---
+++ 07_Reference_datatypes/02_objects.js
@@ -0,0 +1,14 @@
+let obj = {
+    name : "Abhishek",
+    age : 20,
+    height : `5 feet 11inc`,
+    greet : function(){
+        console.log("hello how are you");
+        
+    }
+};
+console.log(obj);
+
+obj.greet();
+
+console.log(typeof(obj));

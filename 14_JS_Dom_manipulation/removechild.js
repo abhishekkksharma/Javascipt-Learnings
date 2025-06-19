0a1cfc6896544e14c6cd628c698e3d904933a640
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-06-19 17:05:42 +0530

    add update css

---
+++ 14_JS_Dom_manipulation/removechild.js
@@ -0,0 +1,5 @@
+let mydiv = document.querySelector('#mydiv');
+
+let parent = document.querySelector('#mydiv');
+let child = document.querySelector('#fpara');
+parent.removeChild(child)
---
+++ 15_Update_css_with_JS/css_change.js
@@ -0,0 +1,4 @@
+// let paraelement = document.getElementById('spara')
+// console.log(paraelement);
+// console.log(paraelement.style)
+// paraelement.style.backgroundColor = 'blue'

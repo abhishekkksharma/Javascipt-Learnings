4818d8b76640ae72b6b32bb70d7328d2fd5d9b61
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-06-21 21:37:11 +0530

    add event loop

---
+++ 18_Event_loop/01_asynchronisation.js
@@ -0,0 +1,55 @@
+console.log("start");
+
+function saymyname() {
+    console.log("This is function call");
+}
+
+setTimeout(saymyname,5000);
+
+console.log("End");
+
+
+//Output :
+// start
+// End
+// This is function call // printed after 5 secs
+
+
+
+/*                ┌──────────────────────┐
+                │      JavaScript      │
+                │      Engine          │
+                └──────────────────────┘
+                          │
+                          ▼
+                    ┌────────────┐
+                    │ Call Stack│◄─────┐
+                    └────────────┘      │
+                          │             │
+  ┌──────────────────┐    │             │
+  │ Web APIs (Timer) │────┘             │
+  └──────────────────┘                  │
+                          ▼             │
+                ┌──────────────────────┐│
+                │  Callback Queue      │┘
+                └──────────────────────┘
+                          │
+                    Event Loop
+                          ▼
+                    Pushes callback
+                    to Call Stack when
+                    stack is empty
+ */
+
+/*
+JavaScript has:
+
+Call Stack – executes function calls.
+
+Web APIs – handles async tasks (setTimeout, DOM events, etc.)
+
+Callback Queue – stores callbacks from Web APIs (like from setTimeout).
+
+Microtask Queue – stores Promise callbacks (like .then, async/await).
+*/
+
